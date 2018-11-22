64802856a755ffc485c0266c2536646ad79aaa02
SevaniDileesha <32263957+SevaniDileesha@users.noreply.github.com>
2018-11-22 09:46:39 +0530

    My first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { LoginPage } from '../login/login';
+import { RegisterPage } from '../register/register';
+
+
+
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+
+  constructor(public navCtrl: NavController) {
+
+  }
+  onGoToLogin(){
+   this.navCtrl.push(LoginPage);
+  }
+  onGoToRegister(){
+    this.navCtrl.push(RegisterPage);
+  }
+
+
+}
---
+++ src/pages/register/register.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams,AlertController } from 'ionic-angular';
+
+import { HomePage } from '../home/home';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Camera , CameraOptions } from '@ionic-native/camera';
+
+
+
+/**
+ * Generated class for the RegisterPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-register',
+  templateUrl: 'register.html',
+})
+export class RegisterPage {
+
+	myphoto:any;
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,private camera:Camera) {
+
+
+}
+
+
+
+
+
+
+
+  onGoToRegister(){
+    this.navCtrl.push(HomePage);
+  }
+
+  takephoto(){
+  	const options: CameraOptions = {
+	  quality: 70,
+	  destinationType: this.camera.DestinationType.FILE_URI,
+	  encodingType: this.camera.EncodingType.JPEG,
+	  mediaType: this.camera.MediaType.PICTURE
+	}
+
+	this.camera.getPicture(options).then((imageData) => {
+	 // imageData is either a base64 encoded string or a file URI
+	 // If it's base64 (DATA_URL):
+	 this.myphoto = 'data:image/jpeg;base64,' + imageData;
+	}, (err) => {
+	 // Handle error
+	});
+  }
+
+
+
+
+}
